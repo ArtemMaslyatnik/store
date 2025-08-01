1979160522967d3ed4c06fb6f97bcad2826f7817
Lykov Artem <maslytnik@gmail.com>
2025-08-01 16:09:04 +0300

    dev navbar

--- front/src/index.js
+++ front/src/index.js
@@ -2,7 +2,7 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserStore from "./store/UserStore";
-
+import DeviceStore from "./store/DeviceStore"
 export const Context = createContext(null)
 
 
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <Context.Provider value={{
       user: new UserStore(),
-\
+      device: new DeviceStore(),
     }}>
       <App />
     </Context.Provider>
